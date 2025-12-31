c367c1afd07fc07e1e6c3ef2f806c45d50b9f12a
chanuka <hecdinuwan@gmail.com>
2026-01-01 02:23:25 +0530

    feat: Initial commit

---
+++ src/main/java/org/example/springboot4withopentelemetry/Springboot4WithOpenTelemetryApplication.java
@@ -0,0 +1,13 @@
+package org.example.springboot4withopentelemetry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot4WithOpenTelemetryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Springboot4WithOpenTelemetryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/springboot4withopentelemetry/Springboot4WithOpenTelemetryApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.springboot4withopentelemetry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot4WithOpenTelemetryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
